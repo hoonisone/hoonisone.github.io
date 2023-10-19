72b2f9a78d68293d069b7ca97a25b1fd6691e723
hoonison <hoonisone@gmail.com>
2023-10-19 17:00:58 +0900

    data load

--- word_note/main.js
+++ word_note/main.js
@@ -7,19 +7,71 @@ answer_box.innerText = "Script"
 const answer_button = document.getElementById('answer_button');
 const prev_button = document.getElementById('prev_button');
 const next_button = document.getElementById('next_button');
+const index_view = document.getElementById('index_view');
+
  
 const buttonClickHandler = () =>{
     script_box.innerText = "Undated"
 };
- 
+var idx = 0
+var len = 0
+var contents = null
 // const jsonData= require('./students.json'); 
 // console.log(jsonData);
+const headers = new Headers({
+    'Content-Type': 'text/xml',
+  });
 
-fetch("C://word.json")
+fetch("https://hoonisone.github.io/word_note/word.json")
 .then(response => {
    return response.json();
 }) 
-.then(jsondata => console.log(jsondata));
+.then(jsondata => {
+    contents = jsondata
+    len = jsondata.length
+    new_content(idx)
+});
 
-answer_button.addEventListener('click',buttonClickHandler);
+answer_button.addEventListener('click', show_answer);
+next_button.addEventListener('click',next);
 //button.removeEventListener('click', buttonClickHandler);
+
+function new_content(idx){
+    script_box.textContent = contents[idx]["Script"]
+    answer_box.textContent = ""
+    index_view.innerHTML = `${idx+1}/${len}`
+}
+
+function set_answer(idx){
+    answer_box.textContent = maks_answer_script(contents[idx]["Words"])
+}
+
+function show_answer(){
+    set_answer(idx)
+}
+
+function next(){
+    if(contents == null)
+        return;
+
+    idx = (idx+1)%len;
+    new_content(idx);
+}
+
+function prev(){
+    if(contents == null)
+        return;
+
+    idx = (idx+len-1)%len;
+    new_content(idx);
+}
+
+function maks_answer_script(words){
+    var answer = ""
+    for (var x in words){
+        word = words[x][0]
+        meaning = words[x][1]
+        answer += `${word}: ${meaning}\n`
+    }
+    return answer
+}
